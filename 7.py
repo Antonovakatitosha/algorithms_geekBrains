9c1b4a27eca689360a8c8ae22ff9620c9de10acf
Kate Evdochenko <Antonovakatitosha@gmail.com>
2021-09-03 00:51:00 +0300

    3 lesson

--- 7.py
+++ 7.py
@@ -8,5 +8,6 @@ for elem in a:
     if elem <= first_min:
         second_min = first_min
         first_min = elem
+    elif elem <= second_min: second_min = elem
 
 print(f"First min = {first_min}, second = {second_min}")
