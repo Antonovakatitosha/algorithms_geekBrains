0591934c140f89901d0a0c989c5aa621477411b5
Kate Evdochenko <Antonovakatitosha@gmail.com>
2021-09-05 02:39:21 +0300

    6 lesson

--- 1.py
+++ 1.py
@@ -1,12 +1,13 @@
 # Версия python 3.8, разрядность системы 64x
 #
+# ЗАДАЧА 1
 #
-# start – 24 bit
-# end – 24 bit
-# i – 24 bit
-# j – 24 bit
+# start – 24 bytes
+# end – 24 bytes
+# i – 24 bytes
+# j – 24 bytes
 #
-# суммарно 56 bit, если под цикл не выделяется память, range итератор, как я поняла
+# суммарно 56 bytes, если под цикл не выделяется память, range итератор, как я поняла
 
 start = 32
 end = 127
@@ -17,3 +18,25 @@ for i in range(start, end + 1, 10):
         print(f'{i + j}: "{chr(i + j)}"', end=', ')
 
     print()
+
+# ЗАДАЧА 2
+#
+# a – 40 + (8 + 24) * 15 = 520 bytes
+# max_negative – 24 bytes
+# position – 24 bytes
+# i – 24 bytes
+#
+# суммарно 592 bytes
+
+from random import randint
+a = [randint(-20, 20) for _ in range(15)]
+print(a)
+
+max_negative = -float("inf")
+position = None
+
+for i in range(len(a)):
+    if 0 > a[i] > max_negative:
+        max_negative = a[i]
+        position = i
+print(f'a[{position}] = {max_negative}')
